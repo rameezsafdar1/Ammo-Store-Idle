1c0b72a18323868d06df89c6f0002dd3767260ef
RamizMakesGames <57453901+rameezsafdar1@users.noreply.github.com>
2022-09-28 19:28:09 +0500

    Tweaks

--- Idle Ammon Tycoon/Assets/Scripts/Task Stations/WeaponStationHelper.cs
+++ Idle Ammon Tycoon/Assets/Scripts/Task Stations/WeaponStationHelper.cs
@@ -49,7 +49,7 @@ public class WeaponStationHelper : MonoBehaviour
                     {
                         onContractSigned.Invoke();
                     }
-
+                    tempFillTime = 0;
                 }
             }
         }
