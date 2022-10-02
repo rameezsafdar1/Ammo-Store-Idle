ed175723bbf60dd876ed4f2b0f907e8facc2c45f
RamizMakesGames <57453901+rameezsafdar1@users.noreply.github.com>
2022-10-02 18:03:20 +0500

    Updates

--- Idle Ammon Tycoon/Assets/Scripts/Managers/GameManager.cs
+++ Idle Ammon Tycoon/Assets/Scripts/Managers/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public bool debug;
-    public int forceCash;
+    public int forceCash, forceGems;
     public GameObject[] Levels;
     public int currentLevel;
 
@@ -27,6 +27,7 @@ public class GameManager : MonoBehaviour
         if (debug)
         {
             saveManager.Instance.addCash(forceCash);
+            saveManager.Instance.addGem(forceGems);
         }
     }
 
