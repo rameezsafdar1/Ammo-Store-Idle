379dc559847312572a1beb2b1cb54b45da574841
RamizMakesGames <57453901+rameezsafdar1@users.noreply.github.com>
2022-10-04 20:04:04 +0500

    Updates

--- Idle Ammon Tycoon/Assets/Scripts/Trigger Points/TriggerPoint.cs
+++ Idle Ammon Tycoon/Assets/Scripts/Trigger Points/TriggerPoint.cs
@@ -8,9 +8,14 @@ public class TriggerPoint : MonoBehaviour
     public Color spriteColor, spriteColor2;
     public TMPro.TextMeshPro text;
     public Collider col;
+    public GameObject pointer;
 
     public void activate()
     {
+        if (pointer != null)
+        {
+            pointer.gameObject.SetActive(true);
+        }
         col.enabled = true;
         for (int i = 0; i < sprite.Length; i++)
         {
@@ -21,6 +26,10 @@ public class TriggerPoint : MonoBehaviour
 
     public void deactivate()
     {
+        if (pointer != null)
+        {
+            pointer.gameObject.SetActive(false);
+        }
         col.enabled = false;
         for (int i = 0; i < sprite.Length; i++)
         {
